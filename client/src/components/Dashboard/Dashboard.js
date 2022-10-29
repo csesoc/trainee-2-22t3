2509526accf356b49d77a546be5d92689038cdf9
eklavya-joshi <eklavya.joshi@protonmail.com>
2022-10-30 00:07:21 +1100

    Worked on task creator

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,11 +1,11 @@
-import { Typography, TextField, Card, CardActions, CardContent, Box, Slider, Divider } from '@mui/material';
-import { styled } from '@mui/system';
-import { LocalizationProvider, CalendarPicker, PickersDay } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
-import { useState, useEffect } from 'react';
-import { TaskCard } from './TaskCards';
-import './Dashboard.css';
+import { Typography, TextField, Card, CardActions, CardContent, Box, Slider, Divider } from "@mui/material";
+import { styled } from "@mui/system";
+import { LocalizationProvider, CalendarPicker, PickersDay } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import { useState, useEffect } from "react";
+import { TaskCard } from "./TaskCards";
+import "./Dashboard.css";
 
 
 export default function Dashboard() {
@@ -13,8 +13,8 @@ export default function Dashboard() {
   const [pickedDate, setPickedDate] = useState(dayjs(new Date()));
   const [dataTasks, setDataTasks] = useState([]);
   const [taskProgress, setTaskProgress] = useState([]);
-  const [startDate, setStartDate] = useState(dayjs(new Date()).format('DD/MM/YYYY'));
-  const [endDate, setEndDate] = useState(dayjs(new Date()).format('DD/MM/YYYY'));
+  const [startDate, setStartDate] = useState(dayjs(new Date()).format("DD/MM/YYYY"));
+  const [endDate, setEndDate] = useState(dayjs(new Date()).format("DD/MM/YYYY"));
 
   let currentWeek = 7;
 
@@ -47,23 +47,23 @@ export default function Dashboard() {
 
   const CustomPickersDay = styled(PickersDay, {
     shouldForwardProp: (prop) =>
-      prop !== 'dayIsBetween' && prop !== 'isFirstDay' && prop !== 'isLastDay',
+      prop !== "dayIsBetween" && prop !== "isFirstDay" && prop !== "isLastDay",
   })(({ theme, dayIsBetween, isFirstDay, isLastDay }) => ({
     ...(dayIsBetween && {
       borderRadius: 0,
       backgroundColor: theme.palette.primary.main,
       color: theme.palette.common.white,
-      '&:hover, &:focus': {
+      "&:hover, &:focus": {
         backgroundColor: theme.palette.primary.dark,
       },
     }),
     ...(isFirstDay && {
-      borderTopLeftRadius: '50%',
-      borderBottomLeftRadius: '50%',
+      borderTopLeftRadius: "50%",
+      borderBottomLeftRadius: "50%",
     }),
     ...(isLastDay && {
-      borderTopRightRadius: '50%',
-      borderBottomRightRadius: '50%',
+      borderTopRightRadius: "50%",
+      borderBottomRightRadius: "50%",
     }),
   }));
 
@@ -72,15 +72,15 @@ export default function Dashboard() {
       return <PickersDay {...pickersDayProps} />;
     }
 
-    const start = pickedDate.startOf('week');
-    const end = pickedDate.endOf('week');
+    const start = pickedDate.startOf("week");
+    const end = pickedDate.endOf("week");
 
-    const dayIsBetween = date.isBetween(start, end, null, '[]');
-    const isFirstDay = date.isSame(start, 'day');
-    const isLastDay = date.isSame(end, 'day');
+    const dayIsBetween = date.isBetween(start, end, null, "[]");
+    const isFirstDay = date.isSame(start, "day");
+    const isLastDay = date.isSame(end, "day");
 
-    setStartDate(dayjs(start).format('DD/MM/YYYY'));
-    setEndDate(dayjs(end).format('DD/MM/YYYY'));
+    setStartDate(dayjs(start).format("DD/MM/YYYY"));
+    setEndDate(dayjs(end).format("DD/MM/YYYY"));
 
     return (
       <CustomPickersDay
@@ -95,8 +95,8 @@ export default function Dashboard() {
 
   return (
     <>
-      <div className='dashboard-container'>
-        <Typography variant="h2" align='center' sx={{fontWeight:"bold"}}>💀 DASHBOARD 💀</Typography>
+      <div className="dashboard-container">
+        <Typography variant="h2" align="center" sx={{fontWeight:"bold"}}>💀 DASHBOARD 💀</Typography>
         <div className="selector-screen">
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <CalendarPicker 
@@ -106,19 +106,35 @@ export default function Dashboard() {
           views={["day", "month"]}
            />
         </LocalizationProvider>
-        <Divider className='date-divider' sx={{mt:0}}>WEEK {currentWeek}<br></br>
-        <Typography className='date-subtext'>{startDate} - {endDate}</Typography>
+        <div className="weekly-stats">
+        <Divider className="week-divider" sx={{mt:0}}>WEEK {currentWeek}<br></br>
+        <Typography className="week-subtext">{startDate} - {endDate}</Typography>
         </Divider>
+        <Box className="week-box">
+          <Typography className="weekly-box-text">Tasks done:</Typography>
+          <Divider className="weekly-box-divider"></Divider>
+          <Typography className="weekly-box-text">Tasks left:</Typography>
+          <Divider className="weekly-box-divider"></Divider>
+          <Typography className="weekly-box-text">Weekly doom:</Typography>
+          <Divider className="weekly-box-divider"></Divider>
+          <Typography className="weekly-box-text">Improvement:</Typography>
+        </Box>
         </div>
-        <Divider className='divider'>LECTURES</Divider>
+        <Box className="create-task-box">
+          Create Task
+          <Divider className="create-task-box-divider"></Divider>
+
+        </Box>
+        </div>
+        <Divider className="divider">LECTURES</Divider>
         <div className="divider-container">
         {TaskCardList("lecture")}
         </div>
-        <Divider className='divider'>TUTORIALS</Divider>
+        <Divider className="divider">TUTORIALS</Divider>
         <div className="divider-container">
         {TaskCardList("tutorial")}
         </div>
-        <Divider className='divider'>HOMEWORK</Divider>
+        <Divider className="divider">HOMEWORK</Divider>
         <div className="divider-container">
         {TaskCardList("homework")}
         </div>
