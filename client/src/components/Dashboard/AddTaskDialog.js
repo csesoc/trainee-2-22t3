04a7b929ee03817c541efd7547af340ac34c2160
eklavya-joshi <eklavya.joshi@protonmail.com>
2022-11-12 21:27:12 +1100

    Did more work

--- client/src/components/Dashboard/AddTaskDialog.js
+++ client/src/components/Dashboard/AddTaskDialog.js
@@ -20,22 +20,63 @@ export function AddTaskDialog(open, taskType) {
     <DialogTitle>Create new {taskType}</DialogTitle>
     <DialogContent>
     <div className="dialog-box">
+      <div className="text-field-box">
+      <InputLabel className="dialog-box-text">Name</InputLabel>
       <TextField
         autoFocus
         margin="dense"
         id="name"
-        label="Name"
         type="text"
         fullWidth
       />
+      </div>
+      <div className="text-field-box">
+      <InputLabel className="dialog-box-text">Course ID</InputLabel>
+      <TextField
+        autoFocus
+        margin="dense"
+        id="courseId"
+        type="text"
+        fullWidth
+      />
+      </div>
       <div className="select-box">
-      <InputLabel className="select-box-text">Task Type</InputLabel>
+      <InputLabel className="dialog-box-text">Task Type</InputLabel>
       <Select
         value={10}
       >
         <MenuItem value={10}>Ten</MenuItem>
         <MenuItem value={20}>Twenty</MenuItem>
         <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+      </div>
+      <div className="select-box">
+      <InputLabel className="dialog-box-text">Term</InputLabel>
+      <Select
+        value={1}
+      >
+        <MenuItem value={1}>One</MenuItem>
+        <MenuItem value={2}>Two</MenuItem>
+        <MenuItem value={3}>Three</MenuItem>
+      </Select>
+      </div>
+      <div className="select-box">
+      <InputLabel className="dialog-box-text">Week</InputLabel>
+      <Select
+        value={1}
+      >
+        <MenuItem value={1}>1</MenuItem>
+        <MenuItem value={2}>2</MenuItem>
+        <MenuItem value={3}>3</MenuItem>
+        <MenuItem value={4}>4</MenuItem>
+        <MenuItem value={5}>5</MenuItem>
+        <MenuItem value={6}>6</MenuItem>
+        <MenuItem value={7}>7</MenuItem>
+        <MenuItem value={8}>8</MenuItem>
+        <MenuItem value={9}>9</MenuItem>
+        <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={11}>11</MenuItem>
+
       </Select>
       </div>
       </div>
